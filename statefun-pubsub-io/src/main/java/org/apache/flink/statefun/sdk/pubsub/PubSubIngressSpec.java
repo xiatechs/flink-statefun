4828c0ec35b6d7768deeae57db9fbdf7b932a7ff
Nick Pocock <pocockn@hotmail.co.uk>
2022-12-12 16:27:34 +0000

    Add beginning of the pub sub ingress spec

---
+++ statefun-pubsub-io/src/main/java/org/apache/flink/statefun/sdk/pubsub/PubSubIngressSpec.java
@@ -0,0 +1,101 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.flink.statefun.sdk.pubsub;
+
+import com.google.auth.Credentials;
+import org.apache.flink.statefun.sdk.IngressType;
+import org.apache.flink.statefun.sdk.io.IngressIdentifier;
+import org.apache.flink.statefun.sdk.io.IngressSpec;
+
+import java.util.Objects;
+import java.util.Properties;
+
+public class PubSubIngressSpec<T> implements IngressSpec<T> {
+    private final Properties properties;
+    private final String projectName;
+    private String subscriptionName;
+
+    private Credentials credentials;
+    private final IngressIdentifier<T> ingressIdentifier;
+
+    PubSubIngressSpec(
+            IngressIdentifier<T> id,
+            Properties properties,
+            String projectName,
+            String subscriptionName,
+            Credentials credentials) {
+        this.properties = requireValidProperties(properties);
+        this.projectName = projectName;
+        this.subscriptionName = subscriptionName;
+
+        this.credentials = credentials;
+        this.ingressIdentifier = Objects.requireNonNull(id);
+    }
+
+    @Override
+    public IngressIdentifier<T> id() {
+        return ingressIdentifier;
+    }
+
+    @Override
+    public IngressType type() {
+        return Constants.PUBSUB_INGRESS_TYPE;
+    }
+
+    public Properties properties() {
+        return properties;
+    }
+
+    public String projectName() {
+        return projectName;
+    }
+
+    public Credentials getCredentials() {
+        return credentials;
+    }
+
+    public String subscriptionName() {
+        return subscriptionName;
+    }
+
+    // TODO: Work out what required properties should go here
+    private static Properties requireValidProperties(Properties properties) {
+        Objects.requireNonNull(properties);
+        return properties;
+    }
+
+    private static String requireSubscriptionName(String subscriptionName) {
+        Objects.requireNonNull(subscriptionName);
+
+        if (subscriptionName.isEmpty()) {
+            throw new IllegalArgumentException("Must define a PubSub subscription to consume from.");
+        }
+
+        return subscriptionName;
+    }
+
+    private static String requireProjectName(String projectName) {
+        Objects.requireNonNull(projectName);
+
+        if (projectName.isEmpty()) {
+            throw new IllegalArgumentException("Must define a PubSub project name.");
+        }
+
+        return projectName;
+    }
+}
